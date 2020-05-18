70884a4929714e7c7128e4302d4dadd64089f899
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2020-05-18 15:52:41 +0200

    Update Main.py

--- Learn_bpm-Python/Main.py
+++ Learn_bpm-Python/Main.py
@@ -97,7 +97,7 @@ def main():
     x = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, -1:].values
     y = OneHotEncoder().fit_transform(y).toarray().astype(float)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2) #shuffle=false
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
     scaler = StandardScaler()
     x_train = scaler.fit_transform(x_train)
     x_test = scaler.fit_transform(x_test)
