f052babb6060a493035072a71ee581daab62d485
martinZz8 <57568383+martinZz8@users.noreply.github.com>
2020-05-18 15:51:54 +0200

    up

--- Learn_bpm-Python/Main.py
+++ Learn_bpm-Python/Main.py
@@ -93,10 +93,11 @@ def printCurrentPercent(rms, number_of_lln, name):
 
 def main():
     dataset = pd.read_csv("winequality-red.csv", sep=';', decimal=",", dtype=np.float)
+    # dataset = dataset.sort_values(by='quality')
     x = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, -1:].values
     y = OneHotEncoder().fit_transform(y).toarray().astype(float)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2) #shuffle=false
     scaler = StandardScaler()
     x_train = scaler.fit_transform(x_train)
     x_test = scaler.fit_transform(x_test)
